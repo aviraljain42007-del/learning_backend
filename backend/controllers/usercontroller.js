ab9b4a87ee1ae2bb320a963726e430f65ac79d00
Aviral Jain <aviraljain42007@gmail.com>
2026-08-14 21:30:00 +0530

    authentication solved

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -27,15 +27,15 @@ exports.login = asyncHandler(async (req, res) => {
 
  res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite:"none",
     maxAge: 15 * 60 * 1000,
 });
 
 res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
